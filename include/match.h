65c4d3492c1d581f1b60742ccea792461a83bbfa
Elie Moussy <elie.moussy@gmail.com>
2017-06-19 19:39:32 +0200

    Add a Match class

--- include/match.h
+++ include/match.h
@@ -1,11 +1,28 @@
 #ifndef MATCH_H
 #define MATCH_H
 
+#include <QString>
 
 class Match
 {
 public:
-    Match();
+    Match(const QString &homeTeam, const QString &awayTeam);
+    Match(const QString &homeTeam, const QString &awayTeam, unsigned int homeScore, unsigned int awayScore);
+
+    inline bool isGamePlayed() const { return aPlayed; }
+    inline void setGamePlayed(bool played=true) { aPlayed = played; }
+
+    bool doesHomeTeamWin();
+    bool doesAwayTeamWin();
+    bool isADraw();
+
+private:
+    bool aPlayed;
+    QString aHomeTeam;
+    QString aAwayTeam;
+
+    unsigned int aHomeScore;
+    unsigned int aAwayScore;
 };
 
 #endif // MATCH_H
--- src/match.cpp
+++ src/match.cpp
@@ -1,6 +1,35 @@
 #include "match.h"
 
-Match::Match()
+Match::Match(const QString &homeTeam, const QString &awayTeam) :
+    aPlayed(false),
+    aHomeTeam(homeTeam),
+    aAwayTeam(awayTeam)
 {
+}
 
+Match::Match(const QString &homeTeam, const QString &awayTeam, unsigned int homeScore, unsigned int awayScore) :
+    aPlayed(true),
+    aHomeTeam(homeTeam),
+    aAwayTeam(awayTeam),
+    aHomeScore(homeScore),
+    aAwayScore(awayScore)
+{
+}
+
+bool Match::doesHomeTeamWin()
+{
+    Q_ASSERT(aPlayed);
+    return aHomeScore > aAwayScore;
+}
+
+bool Match::doesAwayTeamWin()
+{
+    Q_ASSERT(aPlayed);
+    return aHomeScore < aAwayScore;
+}
+
+bool Match::isADraw()
+{
+    Q_ASSERT(aPlayed);
+    return aHomeScore == aAwayScore;
 }
